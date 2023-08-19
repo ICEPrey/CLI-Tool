52f3336ae7e164c835bde5a1cfbb375aa07d4f3b
iceprey <abuwswork@gmail.com>
2023-08-19 13:12:33 -0700

    Change Type To Module & And Highlight With Chalk

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-const arg = require("arg")
+import * as arg from "arg"
+import { chalkStderr } from "chalk"
 try {
   const args = arg({
     "--start": Boolean,
     "--build": Boolean,
   })
   if (args["--start"]) {
-    console.log("test")
+    console.log(chalkStderr.bgCyanBright("starting the app"))
   }
 } catch (e) {
-  console.log(e.message)
+  console.log(chalkStderr.yellow(e.message))
   usage()
 }
 
 function usage() {
-  console.log(`Tool [CMD]
-    --start\tStarts the app
-    --build\tBuilds the app
+  console.log(`${chalkStderr.bgMagentaBright(`Tool [CMD]`)}
+    ${chalkStderr.greenBright(`--start\tStarts the app`)}
+    ${chalkStderr.redBright(`--build\tBuilds the app`)}
     `)
 }
