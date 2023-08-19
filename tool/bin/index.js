e29460bae94e3e3c56a270d08281a0264e6a007b
iceprey <abuwswork@gmail.com>
2023-08-19 13:01:17 -0700

    Learn about arg

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log("hello tool")
+const arg = require("arg")
+try {
+  const args = arg({
+    "--start": Boolean,
+    "--build": Boolean,
+  })
+  if (args["--start"]) {
+    console.log("test")
+  }
+} catch (e) {
+  console.log(e.message)
+  usage()
+}
+
+function usage() {
+  console.log(`Tool [CMD]
+    --start\tStarts the app
+    --build\tBuilds the app
+    `)
+}
